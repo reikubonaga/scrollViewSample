e91ac27ee1d456fc01c95a6cba1f6720e8995a1e
Rei Kubonaga <kubonagarei@gmail.com>
2014-02-10 16:45:37 +0900

    Add scrollView height

--- scrollViewSample/WTDViewController.m
+++ scrollViewSample/WTDViewController.m
@@ -8,7 +8,9 @@
 
 #import "WTDViewController.h"
 
-@interface WTDViewController ()
+@interface WTDViewController () <UIScrollViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -20,10 +22,26 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    [self logScrollView:self.scrollView withString:@"viewWillAppear"];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [self logScrollView:scrollView withString:@"scrollViewDidScroll"];
+}
+
+- (void)logScrollView:(UIScrollView *)scrollView withString:(NSString *)name {
+    NSLog(@"%@ contentOffset x: %f y: %f, contentInset top: %f bottom: %f left: %f right: %f, contentSize height: %f, width: %f", name, scrollView.contentOffset.x, scrollView.contentOffset.y, scrollView.contentInset.top, scrollView.contentInset.bottom, scrollView.contentInset.left, scrollView.contentInset.right, scrollView.contentSize.height, scrollView.contentSize.width);
 }
 
 @end
